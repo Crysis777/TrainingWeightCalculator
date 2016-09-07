7e73d1f63c5f45e2fbc470e6bfe64bf913cff853
Crysis777 <pfeiffer.matthias@student.dhbw-karlsruhe.de>
2016-09-07 18:31:34 +0200

    initial setup

---
+++ app/src/main/java/com/example/trainingweightcalculator/InsertData.java
@@ -0,0 +1,13 @@
+package com.example.trainingweightcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InsertData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_data);
+    }
+}
