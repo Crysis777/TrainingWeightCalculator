5bc9d60273a37111b1687ccd3d182ae6420bdb6b
Crysis777 <pfeiffer.matthias@student.dhbw-karlsruhe.de>
2016-09-08 19:03:45 +0200

    update 1

--- app/src/main/java/com/example/trainingweightcalculator/InsertData.java
+++ app/src/main/java/com/example/trainingweightcalculator/InsertData.java
@@ -2,6 +2,10 @@ package com.example.trainingweightcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class InsertData extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class InsertData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_data);
+
+    }
+
+    public void calculate(View view) {
+        Toast.makeText(this, "This device got toasted", Toast.LENGTH_SHORT).show();
     }
 }
