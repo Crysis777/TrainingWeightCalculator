a91837015882a23bbbe37efd22302acd3c8ec683
Angerer <simon@angererfamily.de>
2016-09-26 13:22:05 +0200

    Änderungen am UI, Textfield

--- app/src/main/java/com/example/trainingweightcalculator/InsertData.java
+++ app/src/main/java/com/example/trainingweightcalculator/InsertData.java
@@ -2,12 +2,22 @@ package com.example.trainingweightcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class InsertData extends AppCompatActivity {
+    static EditText s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_data);
+
+        s = (EditText)findViewById(R.id.editTextWeight);
+
+        getTextFromEditText();
+    }
+
+    static private void getTextFromEditText() {
+        System.out.println(s.getText().toString());
     }
 }
