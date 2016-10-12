1fa28a3ecbc6fa3db986075abda8b231d0bc66da
Angerer <simon@angererfamily.de>
2016-10-12 17:00:13 +0200

    Log Changes

--- app/src/main/java/com/example/trainingweightcalculator/InsertData.java
+++ app/src/main/java/com/example/trainingweightcalculator/InsertData.java
@@ -2,6 +2,7 @@ package com.example.trainingweightcalculator;
 
 import android.content.Intent;
 import android.icu.text.DecimalFormat;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,19 +14,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class InsertData extends AppCompatActivity {
+
+    public static final String LOGTAG = "InsertLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_data);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.option_menu, menu);
+
+        Log.i(LOGTAG, "Optionsmenu was created successfully.");
         return true;
     }
 
@@ -47,32 +55,38 @@ public class InsertData extends AppCompatActivity {
     public void help() {
         Intent i = new Intent(getApplicationContext(), HelpActivity.class);
         startActivity(i);
+        Log.i(LOGTAG, "Help Activity was displayed.");
     }
 
     public void reset() {
-        EditText wEdit  = (EditText)findViewById(R.id.editText);
-        EditText rEdit  = (EditText)findViewById(R.id.editText2);
+        EditText wEdit = (EditText) findViewById(R.id.editText);
+        EditText rEdit = (EditText) findViewById(R.id.editText2);
 
         wEdit.setText("");
         rEdit.setText("");
+
+        Log.i(LOGTAG, "Values have been resetted.");
     }
 
     public void calculate(View view) {
-        EditText wEdit  = (EditText)findViewById(R.id.editText);
-        EditText rEdit  = (EditText)findViewById(R.id.editText2);
+        EditText wEdit = (EditText) findViewById(R.id.editText);
+        EditText rEdit = (EditText) findViewById(R.id.editText2);
 
-        if(wEdit.getText().length() == 0 || rEdit.getText().length() == 0) {
+        if (wEdit.getText().length() == 0 || rEdit.getText().length() == 0) {
             Toast.makeText(this, "Please insert a value.", Toast.LENGTH_SHORT).show();
+            Log.w(LOGTAG, "User did not insert a value.");
             return;
         }
 
-        if(Integer.parseInt(wEdit.getText().toString()) <= 0 || Integer.parseInt(rEdit.getText().toString()) <= 0){
+        if (Integer.parseInt(wEdit.getText().toString()) <= 0 || Integer.parseInt(rEdit.getText().toString()) <= 0) {
             Toast.makeText(this, "Please only insert values greater than zero.", Toast.LENGTH_LONG).show();
+            Log.w(LOGTAG, "User entered a zero value.");
             return;
         }
 
-        if(Integer.parseInt(wEdit.getText().toString()) >= 5000 || Integer.parseInt(rEdit.getText().toString()) >= 30){
+        if (Integer.parseInt(wEdit.getText().toString()) >= 5000 || Integer.parseInt(rEdit.getText().toString()) >= 30) {
             Toast.makeText(this, "Weight must be less than 5000, Reputation less than 30.", Toast.LENGTH_LONG).show();
+            Log.w(LOGTAG, "User entered a wrong value.");
             return;
         }
 
@@ -88,5 +102,7 @@ public class InsertData extends AppCompatActivity {
         i.putExtra("Repetitions", String.valueOf(rEdit.getText()));
 
         startActivity(i);
+
+        Log.i(LOGTAG, "Calculated successfully.");
     }
 }
