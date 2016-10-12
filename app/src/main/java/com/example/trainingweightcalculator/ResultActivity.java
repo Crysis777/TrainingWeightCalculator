598438a9b73aab19d7401872f00a415509ba6b9e
Crysis777 <pfeiffer.matthias@student.dhbw-karlsruhe.de>
2016-10-12 14:13:31 +0200

    2nd activity calculating value display value

--- app/src/main/java/com/example/trainingweightcalculator/ResultActivity.java
+++ app/src/main/java/com/example/trainingweightcalculator/ResultActivity.java
@@ -1,7 +1,11 @@
 package com.example.trainingweightcalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        TextView txtRM = (TextView) findViewById(R.id.textView3);
+
+        Intent i = getIntent();
+
+        String RM = i.getStringExtra("RM");
+        Log.e("Result Activity", RM);
+
+
+        txtRM.setText(RM);
+
     }
 }
